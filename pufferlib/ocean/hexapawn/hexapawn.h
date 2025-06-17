014001d5eea9587a9c4e1faca8b5131ab684cc57
leonardo-toffalini <leonardotoffalini@gmail.com>
2025-06-17 17:13:28 +0200

    wip

--- pufferlib/ocean/hexapawn/hexapawn.h
+++ pufferlib/ocean/hexapawn/hexapawn.h
@@ -16,9 +16,15 @@ typedef struct {
     float n; // Required as the last field 
 } Log;
 
+typedef struct Client {
+    Texture2D black_pawn;
+    Texture2D white_pawn;
+} Client;
+
 // Required that you have some struct for your env
 // Recommended that you name it the same as the env file
 typedef struct {
+  Client* client;
     Log log; // Required field. Env binding code uses this to aggregate logs
     unsigned char* observations; // Required. You can use any obs type, but make sure it matches in Python!
     int* actions; // Required. int* for discrete/multidiscrete, float* for box
@@ -31,6 +37,7 @@ typedef struct {
     int r;
     int c;
 } Hexapawn;
+
 
 typedef struct {
     int r;
@@ -153,7 +160,6 @@ int reached_other_side(Hexapawn* env, int player) {
 
 int make_move(Hexapawn* env, Move move, int player) {
   if (!is_valid_move(env, move, player)) return 0;
-  printf("player: %d, move: (%d, %d) -> (%d, %d)\n", player, move.from.r, move.from.c, move.to.r, move.to.c);
 
   env->observations[p2i(env, move.from)] = EMPTY;
   env->observations[p2i(env, move.to)] = player;
@@ -163,14 +169,12 @@ int make_move(Hexapawn* env, Move move, int player) {
   
   // First check if current player won by reaching other side
   if (reached_other_side(env, player)) {
-    printf("player %d reached other side\n", player);
     env->terminals[0] = 1;
     env->rewards[0] = player == AGENT ? 1 : -1;
     return 1;
   }
   // Then check if opponent has no moves (only if current player hasn't won)
   else if (!opponent_has_moves) {
-    printf("no valid moves left for player %d\n", other_player);
     env->terminals[0] = 1;
     env->rewards[0] = player == AGENT ? 1 : -1;
     return 1;
@@ -219,9 +223,9 @@ void c_step(Hexapawn* env) {
 
   // Agent's move
   if (make_move(env, move, AGENT)) {
-    env->rewards[0] = env->reward_move_valid;
+    env->rewards[0] = 0; // env->reward_move_valid;
   } else {
-    env->rewards[0] = env->reward_move_invalid;
+    env->rewards[0] = -1; // env->reward_move_invalid;
     env->terminals[0] = 1;  // Invalid move ends the game
   }
 
@@ -248,10 +252,21 @@ void c_step(Hexapawn* env) {
   }
 }
 
+Client* make_client(Hexapawn* env) {
+  Client* client = (Client*)calloc(1, sizeof(Client));
+  client->white_pawn = LoadTexture("./pufferlib/resources/hexapawn/white_pawn.png");
+  client->black_pawn = LoadTexture("./pufferlib/resources/hexapawn/black_pawn.png");
+  return client;
+}
+
+void close_client(Client* client) {
+    free(client);
+}
+
 // Required function. Should handle creating the client on first call
 void c_render(Hexapawn* env) {
-  const Color PUFF_BACKGROUND = (Color){27, 27, 27, 255};
-  const Color PUFF_BACKGROUND2 = (Color){13, 13, 13, 255};
+  const Color BG1 = (Color){27, 27, 27, 255};
+  const Color BG2 = (Color){13, 13, 13, 255};
 
   int cell_size = 128;
   int window_width = cell_size * env->size;
@@ -272,17 +287,14 @@ void c_render(Hexapawn* env) {
   }
 
   BeginDrawing();
-  ClearBackground(PUFF_BACKGROUND);
-
-  int board_size = env->size * cell_size;
-
+  ClearBackground(BG1);
 
   // Draw pieces
   for (int i = 0; i < env->size; i++) {
     for (int j = 0; j < env->size; j++) {
       int piece = env->observations[i*env->size + j];
       if ((i + j) % 2 == 0)
-        DrawRectangle(j * cell_size - 1, i * cell_size - 1, cell_size + 1, cell_size + 1, PUFF_BACKGROUND2);
+        DrawRectangle(j * cell_size - 1, i * cell_size - 1, cell_size + 1, cell_size + 1, BG2);
       if (piece == EMPTY) continue;
 
       Color piece_color = (piece == AGENT) ? BLUE : RED;
@@ -290,12 +302,12 @@ void c_render(Hexapawn* env) {
       int center_y = i * cell_size + cell_size/2;
       int radius = cell_size/3;
 
-      // Draw piece shadow
-      DrawCircle(center_x + 2, center_y + 2, radius, (Color){0, 0, 0, 100});
       // Draw piece
       DrawCircle(center_x, center_y, radius, piece_color);
       // Draw piece highlight
-      DrawCircle(center_x - radius/3, center_y - radius/3, radius/3, (Color){255, 255, 255, 100});
+      DrawCircleGradient(center_x - radius/3, center_y - radius/3, radius/3, (Color){255, 255, 255, 100}, (Color){255, 255, 255, 50});
+      // DrawTexture(client->white_pawn, j * cell_size, i * cell_size, piece_color);
+      // void DrawTexture(Texture2D texture, int posX, int posY, Color tint);
     }
   }
 
